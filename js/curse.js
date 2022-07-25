3c7915707f395157b2433e758367733e0977b538
Lucas <lucas.popescu41@gmail.com>
2022-07-26 00:01:04 +0300

    boom am facut tabeluri

--- js/curse.js
+++ js/curse.js
@@ -15,5 +15,15 @@ fetch("https://ergast.com/api/f1/2022/results.json?limit=1000")
     const echipaPilot = constructorCard.querySelector(".echipa-pilot")
     const status = constructorCard.querySelector(".status-status")
     const fastestLap = constructorCard.querySelector(".fastest-lap")
+    titluPagina.textContent = "Rezultate curse" + " " + result.MRData.RaceTable.Races[0].season
+    for(var i = result.MRData.RaceTable.Races.length-1;i>=0;i--){
+        var newConstructor = document.createElement("div")
+        numeCursa.textContent = result.MRData.RaceTable.Races[i].raceName
+        dataCursa.textContent = new Date(result.MRData.RaceTable.Races[i].date).toISOString().replace(/T.*/,'').split('-').reverse().join('-')
+        locCursa.textContent = result.MRData.RaceTable.Races[i].Circuit.Location.country
+        newConstructor.classList.add("tabel-cursa")
+        newConstructor.innerHTML = constructorCard.innerHTML
+        tabelContainer.appendChild(newConstructor)
+    }
     console.log(result.MRData.RaceTable.Races[0])
 })
