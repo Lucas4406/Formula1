4785ca8779ef98eb15903cfac1a7d59f40f21892
Lucas <lucas.popescu41@gmail.com>
2022-07-25 23:51:06 +0300

    html si putin js

--- js/curse.js
+++ js/curse.js
@@ -1,7 +1,19 @@
+const templateCursa = document.querySelector("#curse-template")
+const tabelContainer = document.querySelector(".tabel-container-curse")
+const titluPagina = document.querySelector(".titlu-pagina-curse")
 fetch("https://ergast.com/api/f1/2022/results.json?limit=1000")
 .then(response => 
     response.json()
 )
 .then(result => {
-    console.log(result.MRData.RaceTable.Races[0]);
+    const constructorCard = templateCursa.content.cloneNode(true).children[0]
+    const numeCursa  = constructorCard.querySelector(".nume-cursa")
+    const dataCursa  = constructorCard.querySelector(".data-cursa")
+    const locCursa  = constructorCard.querySelector(".loc-cursa")
+    const pozitie = constructorCard.querySelector(".pozitie")
+    const numePilot = constructorCard.querySelector(".nume-pilot")
+    const echipaPilot = constructorCard.querySelector(".echipa-pilot")
+    const status = constructorCard.querySelector(".status-status")
+    const fastestLap = constructorCard.querySelector(".fastest-lap")
+    console.log(result.MRData.RaceTable.Races[0])
 })
