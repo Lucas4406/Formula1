cdeb0fd6f53e802908ea3429d8aaffd62e2981d2
Lucas <lucas.popescu41@gmail.com>
2022-08-03 14:26:52 +0300

    Am adaugat pagina de stiri!

--- js/stirif1-index.js
+++ js/stirif1-index.js
@@ -1,28 +1,26 @@
-//HEADER
-
 const stiriBtn = document.getElementById("stiri-button")
 stiriBtn.addEventListener("click", ()=>{
-    window.location.assign("pagini/stirif1.html")
+    window.location.assign("/pagini/stirif1.html")
 })
 
 const programBtn = document.getElementById("program-button")
 programBtn.addEventListener("click", ()=>{
-    window.location.assign("pagini/program.html")
+    window.location.assign("/pagini/program.html")
 })
 
 const echipeBtn = document.getElementById("echipe-button")
 echipeBtn.addEventListener("click", ()=>{
-    window.location.assign("pagini/echipe.html")
+    window.location.assign("/pagini/echipe.html")
 })
 
 const istorieBtn = document.getElementById("istorie-button")
 istorieBtn.addEventListener("click", ()=>{
-    window.location.assign("pagini/istorie.html")
+    window.location.assign("/pagini/istorie.html")
 })
 
 const logoBtn = document.querySelector("#logo-button")
 logoBtn.addEventListener("click", ()=>{
-    window.location.assign("index.html")
+    window.location.assign("/index.html")
 })
 
 const fiaBtn = document.querySelector(".fia")
