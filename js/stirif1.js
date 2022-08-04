8884db8390b0938aca808ae9a065aed7440c4e80
Lucas <lucas.popescu41@gmail.com>
2022-08-04 14:21:46 +0300

    ms

--- js/stirif1.js
+++ js/stirif1.js
@@ -1,27 +1,32 @@
-const stiritemplate  = document.querySelector("#template")
-const contentGrid = document.querySelector(".content-grid")
-fetch("https://aqueous-escarpment-24578.herokuapp.com/https://f1scraper44.herokuapp.com/stiri-translate")
-.then(response => 
-    response.json()
-)
-.then(data => {
-    const card = stiritemplate.content.cloneNode(true).children[0]
-    const stireText = card.querySelector("#stiretext")
-    const pozaStire = card.querySelector("#stirephoto")
-    for(var i=0;i<data.length;i++){
-            var newCard = document.createElement("a")
-            stireText.textContent = data[i].titlu
-            pozaStire.src = `${data[i].poza}`
-            newCard.href=data[i].linkuri
-            newCard.target = "_blank"
-            newCard.classList.add("ltag")
-            newCard.innerHTML = card.innerHTML
-            contentGrid.appendChild(newCard)
-            darkMode = localStorage.getItem("darkMode")
-            if(darkMode !== "enabled"){
-                enableDarkMode()
-            } else{
-                disableDarkMode()
-            }
+async function bruv () {
+    const stiritemplate  = document.querySelector("#template")
+    const contentGrid = document.querySelector(".content-grid")
+    var j
+    for(j=0;j<22;j++){
+        var link = "https://aqueous-escarpment-24578.herokuapp.com/https://f1scraper44.herokuapp.com/stiri-translate/" + j
+        await fetch(link)
+        .then(response => 
+            response.json()
+            )
+            .then(data => {
+                const card = stiritemplate.content.cloneNode(true).children[0]
+                const stireText = card.querySelector("#stiretext")
+                const pozaStire = card.querySelector("#stirephoto")
+                var newCard = document.createElement("a")
+                stireText.textContent = data.titlu
+                pozaStire.src = `${data.poza}`
+                newCard.href=data.linkuri
+                newCard.target = "_blank"
+                newCard.classList.add("ltag")
+                newCard.innerHTML = card.innerHTML
+                contentGrid.appendChild(newCard)
+                darkMode = localStorage.getItem("darkMode")
+                if(darkMode === "enabled"){
+                    enableDarkMode()
+                } else{
+                    disableDarkMode()
+                }
+        })
     }
-})
+}
+window.document.onload = bruv()
